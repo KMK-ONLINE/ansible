aec1927df31950b418aa71d445d7a14e1b60714c
Yap Sok Ann <sokann@gmail.com>
2017-04-25 12:06:07 +0700

    Bump local version label

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('requirements.txt') as requirements_file:
 
 setup(
     name='ansible',
-    version=__version__ + '+20170425',
+    version=__version__ + '+20170425.1',
     description='Radically simple IT automation',
     author=__author__,
     author_email='info@ansible.com',
