4397ae39dbd2ba5c9e7f215e709e720739fa00a6
Yap Sok Ann <yap@kmkonline.co.id>
2017-09-27 13:57:56 -0400

    gce_net: Add check mode support

--- lib/ansible/modules/cloud/google/gce_net.py
+++ lib/ansible/modules/cloud/google/gce_net.py
@@ -332,7 +332,8 @@ def main():
             subnet_name=dict(),
             subnet_region=dict(),
             subnet_desc=dict(),
-        )
+        ),
+        supports_check_mode=True
     )
 
     if not HAS_LIBCLOUD:
@@ -386,15 +387,17 @@ def main():
             if mode != 'legacy':
                 kwargs['mode'] = mode
 
-            try:
-                network = gce.ex_create_network(name, *args, **kwargs)
-                json_output['name'] = name
-                json_output['ipv4_range'] = ipv4_range
-                changed = True
-            except TypeError:
-                module.fail_json(msg="Update libcloud to a more recent version (>1.0) that supports network 'mode' parameter", changed=False)
-            except Exception as e:
-                module.fail_json(msg=unexpected_error_msg(e), changed=False)
+            json_output['name'] = name
+            json_output['ipv4_range'] = ipv4_range
+            changed = True
+
+            if not module.check_mode:
+                try:
+                    network = gce.ex_create_network(name, *args, **kwargs)
+                except TypeError:
+                    module.fail_json(msg="Update libcloud to a more recent version (>1.0) that supports network 'mode' parameter", changed=False)
+                except Exception as e:
+                    module.fail_json(msg=unexpected_error_msg(e), changed=False)
 
         if (subnet_name or ipv4_range) and not subnet and mode == 'custom':
             if not hasattr(gce, 'ex_create_subnetwork'):
@@ -402,13 +405,15 @@ def main():
             if not subnet_name or not ipv4_range or not subnet_region:
                 module.fail_json(msg="subnet_name, ipv4_range, and subnet_region required for custom mode", changed=changed)
 
-            try:
-                subnet = gce.ex_create_subnetwork(subnet_name, cidr=ipv4_range, network=name, region=subnet_region, description=subnet_desc)
-                json_output['subnet_name'] = subnet_name
-                json_output['ipv4_range'] = ipv4_range
-                changed = True
-            except Exception as e:
-                module.fail_json(msg=unexpected_error_msg(e), changed=changed)
+            json_output['subnet_name'] = subnet_name
+            json_output['ipv4_range'] = ipv4_range
+            changed = True
+
+            if not module.check_mode:
+                try:
+                    subnet = gce.ex_create_subnetwork(subnet_name, cidr=ipv4_range, network=name, region=subnet_region, description=subnet_desc)
+                except Exception as e:
+                    module.fail_json(msg=unexpected_error_msg(e), changed=changed)
 
         if fwname:
             # user creating a firewall rule
@@ -474,21 +479,23 @@ def main():
                         fw_changed = True
 
                 if fw_changed is True:
-                    try:
-                        gce.ex_update_firewall(fw)
-                        changed = True
-                    except Exception as e:
-                        module.fail_json(msg=unexpected_error_msg(e), changed=False)
+                    changed = True
+                    if not module.check_mode:
+                        try:
+                            gce.ex_update_firewall(fw)
+                        except Exception as e:
+                            module.fail_json(msg=unexpected_error_msg(e), changed=False)
 
             # Firewall rule not found so we try to create it.
             except ResourceNotFoundError:
-                try:
-                    gce.ex_create_firewall(fwname, allowed_list, network=name,
-                                           source_ranges=src_range, source_tags=src_tags, target_tags=target_tags)
-                    changed = True
+                changed = True
+                if not module.check_mode:
+                    try:
+                        gce.ex_create_firewall(fwname, allowed_list, network=name,
+                            source_ranges=src_range, source_tags=src_tags, target_tags=target_tags)
 
-                except Exception as e:
-                    module.fail_json(msg=unexpected_error_msg(e), changed=False)
+                    except Exception as e:
+                        module.fail_json(msg=unexpected_error_msg(e), changed=False)
 
             except Exception as e:
                 module.fail_json(msg=unexpected_error_msg(e), changed=False)
@@ -510,8 +517,9 @@ def main():
             except Exception as e:
                 module.fail_json(msg=unexpected_error_msg(e), changed=False)
             if fw:
-                gce.ex_destroy_firewall(fw)
                 changed = True
+                if not module.check_mode:
+                    gce.ex_destroy_firewall(fw)
         elif subnet_name:
             if not hasattr(gce, 'ex_get_subnetwork') or not hasattr(gce, 'ex_destroy_subnetwork'):
                 module.fail_json(msg='Update libcloud to a more recent version (>1.0) that supports subnetwork creation', changed=changed)
@@ -524,8 +532,9 @@ def main():
             except Exception as e:
                 module.fail_json(msg=unexpected_error_msg(e), changed=False)
             if subnet:
-                gce.ex_destroy_subnetwork(subnet)
                 changed = True
+                if not module.check_mode:
+                    gce.ex_destroy_subnetwork(subnet)
         elif name:
             json_output['name'] = name
             network = None
@@ -537,11 +546,12 @@ def main():
             except Exception as e:
                 module.fail_json(msg=unexpected_error_msg(e), changed=False)
             if network:
-                try:
-                    gce.ex_destroy_network(network)
-                except Exception as e:
-                    module.fail_json(msg=unexpected_error_msg(e), changed=False)
                 changed = True
+                if not module.check_mode:
+                    try:
+                        gce.ex_destroy_network(network)
+                    except Exception as e:
+                        module.fail_json(msg=unexpected_error_msg(e), changed=False)
 
     json_output['changed'] = changed
     module.exit_json(**json_output)
