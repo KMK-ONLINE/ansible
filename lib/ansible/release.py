48761df5c432d31275695217fc14dfcf38e30f04
Yap Sok Ann <yap@kmkonline.co.id>
2017-10-06 06:44:03 -0400

    Bump local version label

--- lib/ansible/release.py
+++ lib/ansible/release.py
@@ -19,5 +19,5 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-__version__ = '2.4.0.0+20170927'
+__version__ = '2.4.0.0+20171006'
 __author__ = 'Ansible, Inc.'
